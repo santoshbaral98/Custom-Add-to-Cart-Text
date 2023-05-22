65669a33e5b47177efeb7fb37e4276c9c3d6a8cb
santoshbaral98 <61633772+santoshbaral98@users.noreply.github.com>
2023-05-22 10:00:51 +0545

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,43 @@
-# Custom-Add-to-Cart-Text
+# Custom Add to Cart Text
+
+## Description
+
+This WordPress plugin allows you to customize the "Add to Cart" text on your WooCommerce store. With a simple interface, you can input the desired text, replacing the default "Add to Cart" label.
+
+## Installation
+
+1. Upload the plugin files to the `/wp-content/plugins/custom-add-to-cart-text` directory, or install the plugin through the WordPress plugins screen directly.
+2. Activate the plugin through the 'Plugins' screen in WordPress.
+3. Use the Settings->Custom Add To Cart Text screen to configure the plugin.
+
+## Usage
+
+1. After activating the plugin, go to the Settings menu in the WordPress admin dashboard.
+2. Click on "Custom Add To Cart Text."
+3. On the settings page, input the desired text for the "Add to Cart" button in the provided text field.
+4. Click "Save Changes." This change will affect all the "Add to Cart" buttons on your WooCommerce store.
+
+## Changelog
+
+= 1.0 =
+* Initial release.
+
+## License
+
+This plugin is licensed under the GPL v2 or later.
+
+    Custom Add to Cart Text - Change the "Add to Cart" text on your WooCommerce store.
+    Copyright (C) 2023  Santosh Baral
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License, version 2, as 
+    published by the Free Software Foundation.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
---
+++ custom-add-to-cart-text.php
@@ -0,0 +1,110 @@
+<?php
+/*
+Plugin Name: Custom Add to Cart Text
+Description: This plugin allows you to customize the "Add to Cart" text on your WooCommerce store.
+Version: 1.0
+Author: Santosh Baral
+Author URI: https://www.linkedin.com/in/santosh-baral-a94019233/
+*/
+
+if (!defined('ABSPATH')) {
+    exit; // Exit if accessed directly
+}
+
+if (!class_exists('Custom_Add_To_Cart_Text')) {
+
+    class Custom_Add_To_Cart_Text
+    {
+        public function __construct()
+        {
+            add_action('admin_menu', array($this, 'add_plugin_page'));
+            add_action('admin_init', array($this, 'page_init'));
+            add_filter('woocommerce_product_single_add_to_cart_text', array($this, 'custom_cart_button_text')); 
+            add_filter('woocommerce_product_add_to_cart_text', array($this, 'custom_cart_button_text')); 
+        }
+
+        public function add_plugin_page()
+        {
+            add_options_page(
+                'Settings Admin', 
+                'Custom Add To Cart Text', 
+                'manage_options', 
+                'custom-add-to-cart-text', 
+                array($this, 'create_admin_page')
+            );
+        }
+
+        public function create_admin_page()
+        {
+            $this->options = get_option('custom_cart_text');
+            ?>
+            <div class="wrap">
+                <h1>Custom Add To Cart Text</h1>
+                <form method="post" action="options.php">
+                <?php
+                    settings_fields('custom_cart_text_group');
+                    do_settings_sections('custom-add-to-cart-text-setting-admin');
+                    submit_button(); 
+                ?>
+                </form>
+            </div>
+            <?php
+        }
+
+        public function page_init()
+        {        
+            register_setting(
+                'custom_cart_text_group',
+                'custom_cart_text',
+                array($this, 'sanitize')
+            );
+
+            add_settings_section(
+                'setting_section_id',
+                'Settings',
+                array($this, 'print_section_info'),
+                'custom-add-to-cart-text-setting-admin'
+            );  
+
+            add_settings_field(
+                'cart_button_text',
+                'Add To Cart Text',
+                array($this, 'cart_button_text_callback'),
+                'custom-add-to-cart-text-setting-admin',
+                'setting_section_id'
+            );      
+        }
+
+        public function sanitize($input)
+{
+    return array(
+        'cart_button_text' => sanitize_text_field($input['cart_button_text'])
+    );
+}
+
+
+        public function print_section_info()
+        {
+            print 'Enter your settings below:';
+        }
+
+        public function cart_button_text_callback()
+        {
+            printf(
+                '<input type="text" id="cart_button_text" name="custom_cart_text[cart_button_text]" value="%s" />',
+                isset($this->options['cart_button_text']) ? esc_attr($this->options['cart_button_text']) : ''
+            );
+        }
+
+        function custom_cart_button_text() 
+        {
+            return $this->options['cart_button_text'] ?: 'Add to Cart';
+        }
+
+    }
+}
+
+if(is_admin()) {
+    $custom_add_to_cart_text = new Custom_Add_To_Cart_Text();
+}
+?>
